0479fe2a0ff551fcb8cb1b149ab3b4576dde3bf9
Kelly <knzegela@avans.nl>
2018-05-14 12:33:07 +0200

    new endpoints

--- routes/Studentenhuis.js
+++ routes/Studentenhuis.js
@@ -16,7 +16,31 @@ router.put('/huisId', (req, res, next) => {
 
 router.delete('/huisId', (req, res, next) => {
 
-})
+});
+
+//
+//maaltijden
+//
+
+router.route('/huisId/maaltijd', (req, res, next) => {
+
+});
+
+router.get('/huisnummer/maaltijd', (req, res, next) => {
+
+});
+
+router.get('/huisnummer/maaltijd/maaltijdId', (req, res, next) => {
+
+});
+
+router.put('/huisnummer/maaltijd/maaltijdId', (req, res, next) => {
+
+});
+
+router.delete('/huisnummer/maaltijd/maaltijdId', (req, res, next) => {
+
+});
 
 // Fall back, display some info
 router.all('*', (req, res) => {
