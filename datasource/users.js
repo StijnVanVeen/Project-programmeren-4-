51ab83e83f61c731166079d4006936905e8f71b5
Stijn van Veen <stijnboz@live.nl>
2018-05-14 14:30:01 +0200

    fix login

---
+++ datasource/users.js
@@ -0,0 +1,16 @@
+user = [
+    {
+        firstname: 'Stijn',
+        lastname: 'van Veen',
+        email: 'stijn@gmail.com',
+        password: '1234'
+    },
+    {
+        firstname: 'Kelly',
+        lastname: 'Zegelaar',
+        email: 'kelly@gmail.com',
+        password: 'qwerty'
+    }
+];
+
+module.exports = user;
