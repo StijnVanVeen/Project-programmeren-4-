f0006e6f4a9d32373974e3393f89d9a16a0678cb
Stijn van Veen <stijnboz@live.nl>
2018-05-15 21:48:17 +0200

    start of register functionality

--- routes/Register.js
+++ routes/Register.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const users = require('../datasource/users');
+const db = require('../datasource/databaseConnection');
 
 
 
 router.route('/')
     .post( function(req, res) {
 
-        var email = req.body.email || '';
-        var password = req.body.password || '';
-        var firstname = req.body.firstname || '';
-        var lastname = req.body.lastname || '';
-              
+        let email = req.body.email || '';
+        let password = req.body.password || '';
+        let firstname = req.body.firstname || '';
+        let lastname = req.body.lastname || '';
 
+        console.log(email, password, firstname, lastname);
 
+        db.query("INSERT INTO user (voornaam, achternaam, email, password) VALUES (?, ?, ?, ?)"), [firstname, lastname, email, password], function (err, result) {
+            if (err) throw err;
+            console.log(result)
+            res.status(200).json(result);
+        }
     });
 
 module.exports = router;
